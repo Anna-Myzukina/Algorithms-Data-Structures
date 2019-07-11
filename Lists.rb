864d584d7311cdc4999a2b95b64b0a4595ade79b
Anna <anna.muzykina83@gmail.com>
2019-07-11 16:54:05 +0300

    Update and rename Arrays and Linked Lists.rb to Lists.rb

--- Lists.rb
+++ Lists.rb
@@ -1,5 +1,55 @@
 =begin 
+
+Lists  
+
+Create your own LinkedList  class for storing integers which should include two methods:  
+add(number) - Add a new Node to the end of the list with a value of number. Nothing needs to be returned. 
+get(index) - Return the value of the Node at position `index` in the List.   
+class Node
+  attr_accessor :value, :next_node
+  
+  def initialize(value, next_node = nil)
+	  @value = value
+    @next_node = next_node
+  end
+end
+
+class LinkedList
+  #setup head and tail
+
+  def add(number)
+    # your code here
+    new_node = Node.new(number)
+	
+  	if @head.nil?
+	  	@head = new_node
+		  @tail = new_node
+	  else
+		  @tail.next_node = new_node
+		  @tail = new_node
+	  end
+  end
+
+  def get(index)
+    #your code here
+    node = @head
+  	
+  	while index > 0 && node
+  	  node = node.next_node
+  	  index -= 1
+  	end
+  	
+  	node.value
+  end
+end
+
+list = LinkedList.new
+list.add(3)
+list.add(5)
+puts list.get(1)
 =end
+
+
 class Node
  attr_accessor :value, :next_node
 

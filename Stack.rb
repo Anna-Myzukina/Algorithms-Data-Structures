731e00d86880afcc6e820b981cf3a2757633d099
Anna <anna.muzykina83@gmail.com>
2019-07-15 12:00:27 +0300

    Update Stack.rb

--- Stack.rb
+++ Stack.rb
@@ -5,46 +5,42 @@ Create a Stack with two methods push and pop. Use your LinkedList class to keep 
 
 
 # Start with your code from LinkedList challenge.
-class Node
-   attr_accessor :value, :next_node
+class Stack
+  def initialize
+    @store = Array.new
+end
+  def push(number)
+    # your code here
+    @store.push(number)
+  end
+  
+  def pop
+    # your code here
+    @store.pop
+  end
+  
+  def min
+    # your code here
+    @store.min
+  end
 
-   def initialize(value=nil, next_node=nil)
-     @value = value
-     @next_node = next_node
-   end
- end
-
- class Stack
-
-   def initialize
-     @head = nil
-   end
-
-   def push(value)
-     @head = Node.new(value, @head)
-   end
-
-   def pop
-     value = @head.value
-     @head = @head.next_node
-     value
-   end
-
- end
+end
 
 stack = Stack.new
-puts stack.push(3)
-puts  stack.push(5)
-puts stack.pop
-# => 5
+stack.push(3)
+stack.push(5)
+puts stack.min
+# => 3
 
-puts stack.push(2)
-puts stack.push(7)
-puts stack.pop
-# => 7
+stack.pop
+stack.push(7)
+puts stack.min
+# => 3
 
-puts stack.pop
+stack.push(2)
+puts stack.min
 # => 2
 
-puts stack.pop
+stack.pop
+puts stack.min
 # => 3

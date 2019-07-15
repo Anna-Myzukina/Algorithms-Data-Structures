de917be8fe76b9c365399d2e2c65efaac11163f9
Anna <anna.muzykina83@gmail.com>
2019-07-15 12:07:04 +0300

    Update Stack.rb

--- Stack.rb
+++ Stack.rb
@@ -1,6 +1,33 @@
 =begin
-Stack
+
+Min Stack
+Create a Stack that contains the standard push and pop methods. 
+It should also contain another method min that returns the minimum number in the Stack.
+
+Extra Credit
+
+All operations should take O(1) time.
+
+Example
+
+This is an example of a test case:
+
+stack = Stack.new
+
+stack.push(3)
+stack.push(5)
+puts stack.min
+# => 3
+
+stack.pop
+stack.push(7)
+puts stack.min
+# => 3
+
+
 Create a Stack with two methods push and pop. Use your LinkedList class to keep track of the elements internally.
+this article explain 
+http://rubymonk.com/learning/books/4-ruby-primer-ascent/chapters/33-advanced-arrays/lessons/86-stacks-and-queues
 =end
 
 

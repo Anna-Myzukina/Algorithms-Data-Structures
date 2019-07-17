5ccde63948b6fff3af7feaa7aa2ba2bb9ac306f9
Anna <anna.muzykina83@gmail.com>
2019-07-17 11:29:39 +0300

    Create Balanced Brackets.rb

---
+++ Balanced Brackets.rb
@@ -0,0 +1,45 @@
+=start
+
+Balanced Brackets
+There are 3 kinds of Brackets: [] {} (). 
+Given a String  of characters, check if all the brackets in the String are Balanced. 
+A  string is balanced if all the start and end brackets are in a correct  
+order so they match each other.
+Here are some balanced Strings:
+
+{}
+(hello)[world]
+[({}{}{})([])]
+
+Here are some Unbalanced ones:
+
+(hello - no ending )
+([)] - The [ is improperly enclosed in the ().
+)( - There's an ending ) without a ( before it.
+
+Return true if a line is balanced and false otherwise.
+
+Examples
+
+The following are 3 test cases:
+
+puts balanced_brackets?('(hello)[world]')
+# => true
+
+puts balanced_brackets?('([)]')
+# => false
+
+puts balanced_brackets?('[({}{}{})([])]')
+# => true
+
+Algorithm:
+
+Declare a character stack S.
+Now traverse the expression string exp.
+If the current character is a starting bracket (‘(‘ or ‘{‘ or ‘[‘) then push it to stack.
+If the current character is a closing bracket (‘)’ or ‘}’ or ‘]’) 
+then pop from stack and if the popped character is the matching starting bracket 
+then fine else parenthesis are not balanced.
+After complete traversal, if there is some starting bracket left in stack then “not balanced”
+Below image is a dry run of the above approach:
+=end

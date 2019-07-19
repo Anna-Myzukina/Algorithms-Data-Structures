9b13df699bb8f6554d8707c3285c29aae5938c62
Anna <anna.muzykina83@gmail.com>
2019-07-19 12:06:34 +0300

    Update MinStack.rb

--- MinStack.rb
+++ MinStack.rb
@@ -32,3 +32,42 @@ stack.pop
 puts stack.min
 # => 3
 =end
+class Stack
+  def initialize
+    @store = Array.new
+end
+  def push(number)
+    # your code here
+    @store.push(number)
+  end
+  
+  def pop
+    # your code here
+    @store.pop
+  end
+  
+  def min
+    # your code here
+    @store.min
+  end
+
+end
+
+stack = Stack.new
+stack.push(3)
+stack.push(5)
+puts stack.min
+# => 3
+
+stack.pop
+stack.push(7)
+puts stack.min
+# => 3
+
+stack.push(2)
+puts stack.min
+# => 2
+
+stack.pop
+puts stack.min
+# => 3

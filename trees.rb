690a79c63e4c4cd02af35f12fa5a063f1aebf02a
Anna <anna.muzykina83@gmail.com>
2019-08-06 11:43:05 +0300

    Create trees.rb

---
+++ trees.rb
@@ -0,0 +1,12 @@
+=begin
+Video about Trees:
+https://www.coursera.org/lecture/data-structures/trees-95qda?authMode=signup&redirectTo=%2Flecture%2Fdata-structures%2Ftrees-95qda
+what binary tree is: 
+https://opendsa-server.cs.vt.edu/ODSA/Books/Everything/html/BinaryTree.html
+
+how to store binary tree in an array (1 as index of the first element): 
+https://inst.eecs.berkeley.edu/~cs61bl/r//cur/trees/array-repr.html?topic=lab20.topic&step=1&course=
+
+how to store a binary tree in an array (0 as index of the first element): 
+https://opendsa-server.cs.vt.edu/ODSA/Books/Everything/html/CompleteTree.html
+=end

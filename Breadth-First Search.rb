8f68406a550b28af25cc03504f854992a898ce2b
Anna <anna.muzykina83@gmail.com>
2019-08-15 11:30:12 +0300

    Create Breadth-First Search.rb

---
+++ Breadth-First Search.rb
@@ -0,0 +1,17 @@
+=begin
+Challenge
+Return an array with the given graph in BFS order.
+
+Sample Input
+graph = {
+  0 => [2], 
+  1 => [4], 
+  2 => [5, 0, 3], 
+  3 => [2], 
+  4 => [1, 5], 
+  5 => [4, 2]
+}
+
+bfs(graph)
+# => [0, 2, 5, 3, 4, 1]
+=end

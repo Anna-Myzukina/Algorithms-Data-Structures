e00c29f91b26367d3e4c125cf625dcd2f99c9ded
Anna <anna.muzykina83@gmail.com>
2019-08-15 11:56:35 +0300

    Create DFS Algorithm.rb

---
+++ DFS Algorithm.rb
@@ -0,0 +1,17 @@
+def depth_first_search(graph, node = 0, visited = [])
+  # write your code here
+  return if visited.include?(node)
+  visited << node
+  graph[node].each { |n| depth_first_search(graph, n, visited) }
+  visited
+end
+
+p depth_first_search({
+  0 => [2], 
+  1 => [4], 
+  2 => [5, 0, 3], 
+  3 => [2], 
+  4 => [1, 5], 
+  5 => [4, 2]
+})
+# => [0, 2, 5, 4, 1, 3]

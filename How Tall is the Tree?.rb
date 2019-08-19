9f796dacc9da2a86329a6cdccd2866f2f0c6ef0d
Anna <anna.muzykina83@gmail.com>
2019-08-19 12:00:47 +0300

    Create How Tall is the Tree?.rb

---
+++ How Tall is the Tree?.rb
@@ -0,0 +1,5 @@
+=brgin
+https://www.freecodecamp.org/news/how-to-calculate-a-binary-trees-height-using-array-iteration-in-ruby-63551c6c65fe/
+https://medium.com/@jarrian.mclean/congrats-you-failed-your-first-code-challenge-1934589da7b9
+
+=end

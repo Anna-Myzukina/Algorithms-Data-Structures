542ee6abe717cc2a27f43c28afa813433d52cc8f
Anna <anna.muzykina83@gmail.com>
2019-10-18 11:14:57 +0300

    Update Is it balanced?.rb

--- Is it balanced?.rb
+++ Is it balanced?.rb
@@ -24,3 +24,36 @@ Get stuck and need some hint?
 Check below link: 
 https://gitlab.com/microverse/guides/coding_challenges/hints/blob/master/challenges/tress_and_graphs/is-it-balanced.md
 =end
+
+class Node
+ attr_reader :data
+ attr_accessor :left, :right
+ def initialize(data)
+   @data = data
+ end
+end
+def array_to_tree(array, i=0)
+ return nil if i >= array.length || array[i] == 0
+ node = Node.new(array[i])
+ node.left = array_to_tree(array, 2*i+1)
+ node.right = array_to_tree(array, 2*i+2)
+ node
+end
+def height(node)
+ return 0 if node.nil?
+ return 1 if node.left == nil && node.right == nil
+[height(node.left), height(node.right)].max + 1
+end
+def balanced_tree? (array)
+ root = array_to_tree(array)
+ stack = [root]
+ until stack.empty?
+   current = stack.pop
+   left_height = height(current.left)
+   right_height = height(current.right)
+   return false if (left_height - right_height).abs > 1
+   stack.push(current.right) unless current.right.nil?
+   stack.push(current.left) unless current.left.nil?
+ end
+ true
+end

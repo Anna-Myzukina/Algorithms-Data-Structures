d09d8454a0c223e02fa83f16c97346d3518fe249
Anna Muzykina <anna.muzykina83@gmail.com>
2019-10-23 11:11:27 +0300

    Create Index Match.rb

---
+++ Index Match.rb
@@ -0,0 +1,12 @@
+=begin
+Index Match
+Given a sorted array of integers with no duplicates, find the spot where array[i] == i.
+It's super-easy to find a solution in O(n) time, but can you code an O(log n) solution?
+
+Challenge
+Return the index where they match, or -1 if there is none.
+
+Example
+puts index_match([0, 2, 3, 7, 9, 11])
+# => 0
+=end

8b1e954931ddbad735a7ed0f336df29d178fa7bd
Anna Muzykina <anna.muzykina83@gmail.com>
2019-10-23 12:04:06 +0300

    Update Index Match.rb

--- Index Match.rb
+++ Index Match.rb
@@ -1,4 +1,9 @@
 =begin
+
+https://stackoverflow.com/questions/10369563/difference-between-on-and-ologn-which-is-better-and-what-exactly-is-olo
+https://www.coursera.org/learn/algorithms-part1/home/welcome
+https://hackernoon.com/10-free-data-structure-and-algorithm-courses-junior-developers-should-explore-978b72871af5
+
 Index Match
 Given a sorted array of integers with no duplicates, find the spot where array[i] == i.
 It's super-easy to find a solution in O(n) time, but can you code an O(log n) solution?

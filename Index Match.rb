f8ab0966206e132393d609fca2a06d38699164db
Anna Muzykina <anna.muzykina83@gmail.com>
2019-10-23 11:19:48 +0300

    Update Index Match.rb

--- Index Match.rb
+++ Index Match.rb
@@ -10,3 +10,22 @@ Example
 puts index_match([0, 2, 3, 7, 9, 11])
 # => 0
 =end
+
+def index_match(array)
+  # write your code here
+  array.each_index.select do |index|
+    if array[index] == index
+      spot = array[index]
+      return spot
+    end
+  end
+  else
+    return -1
+end
+    
+      
+      
+
+
+puts index_match([0, 2, 3, 7, 9, 11])
+# => 0

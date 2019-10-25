aaa9e4bc582efe2cd0919416a440e8c5b1a4f435
Anna Muzykina <anna.muzykina83@gmail.com>
2019-10-25 19:06:59 +0300

    Update Time Scheduler.rb

--- Time Scheduler.rb
+++ Time Scheduler.rb
@@ -7,31 +7,22 @@ Two intervals [i, j] & [k, l] are said to be disjoint if they do not have any po
 
 
 def time_scheduler(array)
-  # write your code here
-  return [] if array.empty?
-  groups = array.group_by { |n| n <=> array.first }
-  less_than    = groups[-1] || []
-  first        = groups[0]  || []
-  greater_than = groups[1]  || []
-  quick_sort(less_than) + first + quick_sort(greater_than)
-  
-end
-  def quick_sort(array)
-  # write your code here
-  first = array[0]
-  right = []
-  left = []
-
-  for i in 1..(array.length - 1)
-    if first > array[i] 
-      left << array[i] 
-    else 
-      right <<  array[i]
+  result =[]
+  events = array.each_slice(2).to_a.sort! {|a, b| a[1] <=> b[1] }
+  result << events[0]
+  r1 = events[0][1]
+  for i in (1...events.size)
+    l1 = events[i][0]
+    r2 = events[i][1]
+    if l1 > r1
+      result << events[i]
+      r1 = r2
     end
   end
-  left + [first] + right
-  
+  result
 end
 
+p time_scheduler([4, 8, 1, 3, 7, 9, 5, 6])
+# => [[1,3], [5,6], [7,9]]
 p time_scheduler([4, 8, 1, 3, 7, 9, 5, 6])
 # => [[1,3], [5,6], [7,9]]

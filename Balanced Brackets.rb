4ade0a05fc4979c2f32efc9d5cd587a6cd010f0f
Anna Muzykina <anna.muzykina83@gmail.com>
2019-11-11 23:30:33 +0200

    Update Balanced Brackets.rb

--- Balanced Brackets.rb
+++ Balanced Brackets.rb
@@ -36,14 +36,14 @@ puts balanced_brackets?('[({}{}{})([])]')
 
 Algorithm:
 
-Declare a character stack S.
-Now traverse the expression string exp.
-If the current character is a starting bracket (‘(‘ or ‘{‘ or ‘[‘) then push it to stack.
-If the current character is a closing bracket (‘)’ or ‘}’ or ‘]’) 
+1.Declare a character stack S.
+2.Now traverse the expression string exp.
+3.If the current character is a starting bracket (‘(‘ or ‘{‘ or ‘[‘) then push it to stack.
+4.If the current character is a closing bracket (‘)’ or ‘}’ or ‘]’) 
 then pop from stack and if the popped character is the matching starting bracket 
 then fine else parenthesis are not balanced.
-After complete traversal, if there is some starting bracket left in stack then “not balanced”
-Below image is a dry run of the above approach:
+5.After complete traversal, if there is some starting bracket left in stack then “not balanced”
+6.Below image is a dry run of the above approach:
 =end
 
 // () - true
